6f0ba3b33f2549779b7275521b144d1f61ab2364
Katie Agresta <katieagresta1@gmail.com>
2019-06-30 12:16:14 -0400

    restarts problems

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,138 +2,30 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
-  }
-}
+class Node {}
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  insertFirst(data) {
-    this.head = new Node(data, this.head);
-  }
+  constructor() {}
+  insertFirst(data) {}
 
-  size() {
-    let count = 0;
-    let currentList = this.head;
-    while (currentList) {
-      count++;
-      currentList = currentList.next;
-    }
-    return count;
-  }
+  size() {}
 
-  getFirst() {
-    return this.head;
-  }
+  getFirst() {}
 
-  getLast() {
-    let currentNode = this.head;
-    while (currentNode) {
-      if (currentNode.next === null) {
-        return currentNode;
-      } else {
-        currentNode = currentNode.next;
-      }
-    }
-  }
-  clear() {
-    this.head = null;
-  }
-  removeFirst() {
-    if (!this.head) {
-      return;
-    }
-    this.head = this.head.next;
-  }
+  getLast() {}
+  clear() {}
+  removeFirst() {}
 
-  removeLast() {
-    if (!this.head) {
-      return;
-    }
+  removeLast() {}
 
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
+  insertLast(data) {}
 
-    let previous = this.head;
-    let node = this.head.next;
-    while (node.next) {
-      previous = node;
-      node = node.next;
-    }
-    previous.next = null;
-  }
+  getAt(idx) {}
 
-  insertLast(data) {
-    const last = this.getLast();
+  removeAt(idx) {}
 
-    if (last) {
-      // There are some existing nodes in our chain
-      last.next = new Node(data);
-    } else {
-      // The chain is empty!
-      this.head = new Node(data);
-    }
-  }
-
-  getAt(idx) {
-    let count = 0;
-    let theNode = this.head;
-    while (theNode) {
-      if (count === idx) {
-        return theNode;
-      }
-      theNode = theNode.next;
-      count++;
-    }
-    return null;
-  }
-
-  removeAt(idx) {
-    if (!this.head) {
-      return;
-    }
-    if (idx === 0) {
-      this.head = this.head.next;
-      return;
-    }
-    const previous = this.getAt(idx - 1);
-    if (!previous || !previous.next) {
-      return;
-    }
-    previous.next = previous.next.next;
-  }
-
-  insertAt(data, index) {
-    if (!this.head) {
-      this.head = new Node(data);
-      return;
-    }
-
-    if (index === 0) {
-      this.head = new Node(data, this.head);
-      return;
-    }
-
-    const previous = this.getAt(index - 1) || this.getLast();
-    const node = new Node(data, previous.next);
-    previous.next = node;
-  }
-  forEach(fn) {
-    let node = this.head;
-    let counter = 0;
-    while (node) {
-      fn(node, counter);
-      node = node.next;
-      counter++;
-    }
-  }
+  insertAt(data, index) {}
+  forEach(fn) {}
 
   *[Symbol.iterator]() {
     let node = this.head;
