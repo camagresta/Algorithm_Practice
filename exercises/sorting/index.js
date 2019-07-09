a6ebf2f2d10ce4afa8ead0f14405584148fe1352
Katie Agresta <katieagresta1@gmail.com>
2019-07-09 13:02:46 -0400

    completes merge function

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -18,6 +18,17 @@ function selectionSort(arr) {}
 
 function mergeSort(arr) {}
 
-function merge(left, right) {}
+function merge(left, right) {
+  let results = [];
+
+  while (left.length || right.length) {
+    if (left[0] === undefined || left[0] > right[0]) {
+      results.push(right.shift());
+    } else {
+      results.push(left.shift());
+    }
+  }
+  return results;
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
