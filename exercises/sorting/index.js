e24448a247bb75fb29d9610559647e75ba029964
Katie Agresta <katieagresta1@gmail.com>
2019-07-11 15:34:44 -0400

    deletes merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -7,17 +7,6 @@ function selectionSort(arr) {}
 
 function mergeSort(arr) {}
 
-function merge(left, right) {
-  let results = [];
-
-  while (left.length || right.length) {
-    if (left[0] === undefined || left[0] > right[0]) {
-      results.push(right.shift());
-    } else {
-      results.push(left.shift());
-    }
-  }
-  return results;
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
